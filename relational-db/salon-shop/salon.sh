aac831d02eb7916691ce82da0d57f3e7059fe1cb
Gguife <gguife747@gmail.com>
2025-02-24 23:03:32 -0300

    Finished freecodecamp Salon exercise

--- relational-db/salon-shop/salon.sh
+++ relational-db/salon-shop/salon.sh
@@ -1,87 +1,53 @@
 #! /bin/bash
 
-#LEMBRAR DE ALTERAR USERNAME PARA freecodecamp
-PSQL='psql -X --username=gguife --dbname=salon --tuples-only-c'
+PSQL="psql -X --username=freecodecamp --dbname=salon --tuples-only -c"
 
-echo -e "\n~~~~ Salon gguife services ~~~~\n"
+echo -e "\n~~~~~ MY SALON ~~~~~\n"
+echo "Welcome to My Salon, how can I help you?"
 
 MAIN_MENU() {
-  if [[ $1 ]]
-  then
+  if [[ $1 ]]; then
     echo -e "\n$1"
   fi
 
-  echo "How may I help you?"
-  echo -e "\n1) Appointment for a woman's haircut \n2) Appointment for a Man's haircut \n3) Exit"
-  read MAIN_MENU_SELECTION
-
-
-  case $MAIN_MENU_SELECTION in 
-    1) SERVICE_SELECTED="Woman Haircut"; BOOK_SERVICE ;;
-    2) SERVICE_SELECTED="Man Haircut"; BOOK_SERVICE ;;
-    3) EXIT ;;
-    *) MAIN_MENU "Please enter a valid option." ;;
+  echo -e "\n1) Cut\n2) Color\n3) Perm\n4) Style\n5) Trim"
+  read SERVICE_ID_SELECTED
+  
+  case $SERVICE_ID_SELECTED in
+    1) SERVICE_NAME="cut" ;;
+    2) SERVICE_NAME="color" ;;
+    3) SERVICE_NAME="perm" ;;
+    4) SERVICE_NAME="style" ;;
+    5) SERVICE_NAME="trim" ;;
+    *) MAIN_MENU "I could not find that service. What would you like today?" ; return ;;
   esac
 
-
-  if [[ $MAIN_MENU_SELECTION -eq 1 || $MAIN_MENU_SELECTION -eq 2 ]] 
-  then
-      echo "You selected: $SERVICE_SELECTED"
-  else
-      MAIN_MENU
-  fi
-
+  BOOK_SERVICE
 }
-
 
 BOOK_SERVICE() {
-  SERVICE_ID_SELECTED=$SERIVICE_SELECTED
-  
-  echo -e "\nWhat is your phone number?"
+  echo -e "\nWhat's your phone number?"
   read CUSTOMER_PHONE
   
-
-
-  # Script for verify if register user exist // verify by phone number 
   CUSTOMER_ID=$($PSQL "SELECT customer_id FROM customers WHERE phone='$CUSTOMER_PHONE'")
+  CUSTOMER_ID=$(echo "$CUSTOMER_ID" | xargs)
 
-
-  # When exist user 
-  if [[ $CUSTOMER_ID ]]
-  then
-    echo -e "\nWhat time do you want to book the service?"
-    read SERVICE_TIME
-    # Register service id in appointments with customer_id
-
-
-    # Register service time in appointments with customer_id
-  fi
-
-  # if user not exist in database
-  if [[ -z $CUSTOMER_ID ]]
-  then
-    echo -e "\nWhat's your name?"
+  if [[ -z "$CUSTOMER_ID" ]]; then
+    echo -e "\nI don't have a record for that phone number, what's your name?"
     read CUSTOMER_NAME
-
-    # Register user  - name and phone number
-    INSERT_CUSTOMER=$($PSQL "INSERT INOT customers(phone, name) VALUES('$CUSTOMER_PHONE', '$CUSTOMER_NAME')")
-
-
-
-    echo -e "\nWhat time do you want to book the service?"
-    read SERVICE_TIME
-
-    # Register id service
-    
-
-    # Register service time 
+    INSERT_CUSTOMER=$($PSQL "INSERT INTO customers(phone, name) VALUES('$CUSTOMER_PHONE', '$CUSTOMER_NAME')")
+    CUSTOMER_ID=$($PSQL "SELECT customer_id FROM customers WHERE phone='$CUSTOMER_PHONE'")
+    CUSTOMER_ID=$(echo "$CUSTOMER_ID" | xargs)
+  else
+    CUSTOMER_NAME=$($PSQL "SELECT name FROM customers WHERE customer_id='$CUSTOMER_ID'")
+    CUSTOMER_NAME=$(echo "$CUSTOMER_NAME" | xargs)
   fi
-}
 
-
-
-EXIT() {
-  echo -e "\nThank you for sptopping in.\n"
+  echo -e "\nWhat time would you like your $SERVICE_NAME, $CUSTOMER_NAME?"
+  read SERVICE_TIME
+  
+  REGISTER_APPOINTMENT=$($PSQL "INSERT INTO appointments(customer_id, service_id, time) VALUES('$CUSTOMER_ID', '$SERVICE_ID_SELECTED', '$SERVICE_TIME')")
+  echo -e "\nI have put you down for a $SERVICE_NAME at $SERVICE_TIME, $CUSTOMER_NAME."
 }
 
 MAIN_MENU
